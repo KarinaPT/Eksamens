edb392ee20fce5c23bcc94397c42d1905bacfd67
Karina <kstifurska@gmail.com>
2021-06-08 15:08:48 +0300

    Pievienoti mainīgie, JFrame(JFrameField, JTextArea, JButton, JLabl) , pievienoja nelielu logu un citie public void

--- src/Quiz_testsarpogam.java
+++ src/Quiz_testsarpogam.java
@@ -1,7 +1,8 @@
-
-public class Quiz_testsarpogam {
-
-	public static void main(String[] args) {
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+public class Quiz_testsarpogam implements ActionListener{
+	
 		String[] Jautajumi = 	{
 				"Jautajums",
 				"Jautajums",
@@ -34,9 +35,53 @@ public class Quiz_testsarpogam {
 				'A',
 				'B',
 				'C',
-				'C'
+				'D'
 			};
+		int sekundes = 20;
+		int rezultats = 0;
+		char atbilde;
+		int index;
+		int minejumi;
+		int parminejumi;
+		int KopJauSk = Jautajumi.length;
+		
+		JFrame frame = new JFrame();
+		JTextField textfield = new JTextField();
+		JTextArea textarea = new JTextArea();
+		JButton PogaA = new JButton();
+		JButton PogaB = new JButton();
+		JButton PogaC = new JButton();
+		JButton PogaD = new JButton();
+		JLabel AtbildeA = new JLabel();
+		JLabel AtbildeB = new JLabel();
+		JLabel AtbildeC = new JLabel();
+		JLabel AtbildeD = new JLabel();
+		JLabel Laiks = new JLabel();
+		JLabel AtlLaiks = new JLabel();
+		JTextField Numurs = new JTextField();
+		JTextField prc = new JTextField();
 
+	public Quiz_testsarpogam(){
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800,800);
+		frame.setVisible(true);
+		
+		
 	}
+	public void NakamajsJautajums() {
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}
+	public void Atbildes() {
+		
+	}
+	public void Rezultats() {
+		
+	}
+	
 
 }
